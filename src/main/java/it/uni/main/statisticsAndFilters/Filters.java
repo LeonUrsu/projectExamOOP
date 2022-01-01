42e68a1c003c1b153a470e0de4efd4b5de0bf218
Leon Dev <leonleonursu@gmail.com>
2022-01-01 16:55:43 +0100

    modifiche non rilevanti Filters

--- src/main/java/it/uni/main/statisticsAndFilters/Filters.java
+++ src/main/java/it/uni/main/statisticsAndFilters/Filters.java
@@ -25,7 +25,7 @@ public class Filters {
 	}
 	
 
-	//TO TEST
+	
 	public CurrentStats dailyFilter(long initialValue, long finalValue, int days)  throws IllegalArgumentException
 	{																							//unix format per gli Value
 		CurrentForecastService currentForecastService = new CurrentForecastService();
@@ -34,7 +34,7 @@ public class Filters {
 		if(tmpVec == null || tmpVec.size() == 0)
 			currentForecastService.apriDaFile(FileReferenceOOPE.myFile, tmpVec );				//IDEA: posso fondere tmpVec con FilteredVec
 		Vector<ForecastDataCurrent> filteredVector = new Vector<ForecastDataCurrent>();
-		confrontaOre(finalValue, finalValue);
+		verifyBand(finalValue, finalValue);
 		daysPeriodFilter(initialValue, finalValue, days, tmpVec, filteredVector); 				//filtraggio TO-TEST
 		StatisticsCurrentForecasts statisticsCurrentForecasts = new StatisticsCurrentForecasts();
 		return statisticsCurrentForecasts.currentStats(initialValue, finalValue, days, filteredVector);
@@ -44,7 +44,7 @@ public class Filters {
 	
 	/**
 	 * Metodo filtro, ci permette di filtrare elementi di un vettore e salvarli in un'altro vettore.
-	 * Il filtraggio avviene per fascia oraria, ad esempio 15:00 - 18:00 scitti in secondi quindi 15*60*60s - 18*60*60s 
+	 * Il filtraggio avviene per fascia oraria, ad esempio 15:00-18:00 scitti in secondi quindi 15*60*60s-18*60*60s 
 	 * inoltre vengono filtrati 'n' giorni desiderati
 	 * 
 	 * @param initialValue tempo iniziale is secondi(s) dalle 0:00
@@ -56,23 +56,17 @@ public class Filters {
 	 */
 	private void daysPeriodFilter(long initialValue, long finalValue, int days, Vector<ForecastDataCurrent> toFilterVector,
 								  Vector<ForecastDataCurrent> filteredVector) throws IllegalArgumentException{
+		
 		//find the biggest dt time in Vector
-		long unix = findBiggestValue(toFilterVector);
-		
-		//look if we can filter 'n' days OR if in tmpVec there are 'n days' of forecasts //TO-TEST
-		if(days!=0) {
-			boolean looked = true;
-			unix -= days * 86400;							//valore minimo accettabile per il filtraggio
-			for(int i=0, u=toFilterVector.size(); i<u ; i++) 
-				if(toFilterVector.get(i).getDayTime() <= unix )
-					looked = false;
-			if(looked) throw new IllegalArgumentException();
-		}	
-		
+		long unixMax = findBiggestValue(toFilterVector);
+		long unixMin = findSmallestValue(toFilterVector);
+		if ( (unixMax - unixMin) < (days * 86400) )
+			throw new IllegalArgumentException();
+	
 		//filtering process	//TO TEST																
 		for(int i=0, u=toFilterVector.size() ; i<u ; i++) {
 			ForecastDataCurrent tmpEle = toFilterVector.get(i);
-			if(controllTimeBand(initialValue, finalValue, unix, tmpEle))
+			if(forecastInTimeBandCheck(unixMin, unixMax, initialValue, finalValue,days*86400 , tmpEle))
 				filteredVector.add(tmpEle);
 		}
 	}
@@ -80,32 +74,35 @@ public class Filters {
 	
 	
 	/**
-	 * Metodo che confronta se la previsone passata rietra nel'intervallo desiderato
+	 * Metodo che confronta se la previsone passata rietra nel'intervallo desiderato	
+	 * @param finalValueDays
+	 * @param initialValueDays
 	 * @param initialValue valore mimimo dell'intervallo
 	 * @param finalValue valore massimo dell'intervallo
-	 * @param unix ?
-	 * @param tmp 
+	 * @param unix
+	 * @param tmp
 	 * @return
 	 */
-	private boolean controllTimeBand(long initialValue, long finalValue, long unix, ForecastDataCurrent tmp)
+	private boolean forecastInTimeBandCheck(long initialValueDays, long finalValueDays, long initialValue, long finalValue, long unix, ForecastDataCurrent tmp)
 	{
-		long tmpTime = tmp.getDayTime() % 86400;								//secondi dalle ore 0:00
-		if(initialValue <= tmpTime && tmpTime <= finalValue && unix < tmpTime)
+		long dt = tmp.getDayTime();
+		long dt24 = dt % 86400;		//secondi dalle ore 0:00
+		if( initialValueDays <= dt && dt <= finalValueDays && initialValue <= dt24 && dt24 <= finalValue )
 			return true;
 		return false;
 	}
 	
 	
-
+	
 	/**
 	 * Trova nel vettore delle previsoni non filtrate il valore unix 
 	 * della data piu grande, quindi quello più recente
-	 * @param vettore
+	 * @param vettore previsioni
 	 * @return long secondi dal "1 gennaio 1970"
 	 */
 	public long findBiggestValue(Vector<ForecastDataCurrent> vettore)
 	{
-		long dt = 0;
+		long dt = vettore.get(0).getDayTime();
 		for(int i=0, u=vettore.size() ; i<u ; i++) 
 			if(dt < vettore.get(i).getDayTime())
 				dt = vettore.get(i).getDayTime();
@@ -116,13 +113,31 @@ public class Filters {
 	
 	
 	/**
-	 * Metodo che verifica la correttezza dei valori di due parametri che sono intesi come tempo in secondi dalle 0:00
+	 * Trova nel vettore delle previsoni non filtrate il valore unix 
+	 * della data piu piccolo, quindi quello meno recente
+	 * @param vettore previsioni
+	 * @return long secondi dal "1 gennaio 1970"
+	 */
+	public long findSmallestValue(Vector<ForecastDataCurrent> vettore)
+	{
+		long dt = vettore.get(0).getDayTime();
+		for(int i=0, u=vettore.size() ; i<u ; i++) 
+			if(dt > vettore.get(i).getDayTime())
+				dt = vettore.get(i).getDayTime();
+		if(dt == 0) throw new IllegalArgumentException();
+		return dt;
+	}
+	
+	
+	
+	/**
+	 * Metodo che verifica la correttezza dei valori di due parametri rientrino nel intervallo orario 0:00:00 -23:59:59
 	 * @param a primo paramtetro 
 	 * @param b secondo parametro
 	 * @throws IllegalArgumentException
 	 */
-	private void confrontaOre(long a, long b) throws IllegalArgumentException{ //si puo creare un tipo illegalTimeException
-		if(a!=b && 0<a && a< 86400 && 1<b && b<= 86400);
+	private void verifyBand(long a, long b) { //si puo creare un tipo illegalTimeException
+		if(a<b && 0<=a && b<= 86399);
 		else throw new IllegalArgumentException();
 	}
 	
